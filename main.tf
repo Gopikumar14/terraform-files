8a20bd5e29b2a4893d55900f96dcb4eb2c668ea9
Gopikumar14 <114049453+Gopikumar14@users.noreply.github.com>
2023-09-06 12:14:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "azurerm_app_service" "appservice" {
   name                = "dotnet-gk-appservice"
   location            = azurerm_app_service_plan.rg.location
   resource_group_name = azurerm_app_service_plan.rg.resource_group_name
-  app_service_plan_id = azurerm_app_service_plan.appservice.id
+  app_service_plan_id = azurerm_app_service_plan.rg.id
 
   site_config {
     dotnet_framework_version = "v4.0"
